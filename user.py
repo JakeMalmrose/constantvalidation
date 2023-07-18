d8ddeb878b6572111a0d909de8caad594ccf2529
JakeMalmrose <jmalmrose@student.neumont.edu>
2023-07-18 09:51:52 -0600

    User now uses validation on initialization

--- user.py
+++ user.py
@@ -1,18 +1,82 @@
 # this class is used to create a user object
-# it will be used to store user data
-# A user will have the following fields:
-# First Name, Last Name, Email, Address, City, State, Zipcode, Phone Number and Password
+# it will be used to store and validate user data
+import validator
 
 class User:
     def __init__(self, firstName, lastName, email, address, city, state, zipcode, phoneNumber, password):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.email = email
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zipcode = zipcode
-        self.phoneNumber = phoneNumber
-        self.password = password
+        self.firstName = None
+        self.lastName = None
+        self.email = None
+        self.address = None
+        self.city = None
+        self.state = None
+        self.zipcode = None
+        self.phoneNumber = None
+        self.password = None
+        self.setFirstName(firstName)
+        self.setLastName(lastName)
+        self.setEmail(email)
+        self.setAddress(address)
+        self.setCity(city)
+        self.setState(state)
+        self.setZipcode(zipcode)
+        self.setPhoneNumber(phoneNumber)
+        self.setPassword(password)
+    
+    def setFirstName(self, firstName):
+        if validator.validateFirstName(firstName):
+            self.firstName = firstName
+            return True
+        return False
+    
+    def setLastName(self, lastName):
+        if validator.validateLastName(lastName):
+            self.lastName = lastName
+            return True
+        return False
+    
+    def setEmail(self, email):
+        if validator.validateEmail(email):
+            self.email = email
+            return True
+        return False
+    
+    def setAddress(self, address):
+        if validator.validateAddress(address):
+            self.address = address
+            return True
+        return False
+    
+    def setCity(self, city):
+        if validator.validateCity(city):
+            self.city = city
+            return True
+        return False
+    
+    def setState(self, state):
+        if validator.validateState(state):
+            self.state = state
+            return True
+        return False
+    
+    def setZipcode(self, zipcode):
+        if validator.validateZipcode(zipcode):
+            self.zipcode = zipcode
+            return True
+        return False
+    
+    def setPhoneNumber(self, phoneNumber):
+        if validator.validatePhoneNumber(phoneNumber):
+            self.phoneNumber = phoneNumber
+            return True
+        return False
+    
+    def setPassword(self, password):
+        if validator.validatePassword(password):
+            self.password = password
+            return True
+        return False
+    
+    
     
     
