6bc31e135ae06c7c413faea170b3b48c542f31d6
JakeMalmrose <jmalmrose@student.neumont.edu>
2023-07-25 19:32:35 -0600

    added regexes for validator, still needs some fixing

--- validator.py
+++ validator.py
@@ -4,20 +4,35 @@
 import re
 
 regexes = {
-    "firstName": "^[a-zA-Z]+{2}$",
-    "lastName": "^[a-zA-Z]{2,50}$"
+    "firstName": "^[a-zA-Z]{2,50}$",
+    "lastName": "^[a-zA-Z]{2,50}$",
+    "email": "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+    "address": "^[a-zA-Z0-9 ]{2,50}$",
+    "city": "^[a-zA-Z]{2,50}$",
+    "state": "^[a-zA-Z]{2}$",
+    "zipcode": "^[0-9]{5}$",
+    "phoneNumber": "^[0-9]{10}$",
+    "password": "^[a-zA-Z0-9]{8,50}$"
 }
 
 def validateInput(input, regex):
-    if re.match(regex, input):
+    if re.match(regex, input, re.IGNORECASE):
         return True
     else:
         return False
-    
-def validatePersonAttributes(personAttributes):
+
+def findInvalidInput(personAttributes):
     for key in personAttributes:
         if not validateInput(personAttributes[key], regexes[key]):
-            return False
+            return key
+    return True
+    
+# PersonAttributes have been validated all together due to assignment
+# requirements. It would probably be preferable to check as it's inputted
+def validatePersonAttributes(personAttributes): 
+    for key in personAttributes:
+        if not validateInput(personAttributes[key], regexes[key]):
+            return False, regexes[key]
     return True
 
 def validatePassword(password):
